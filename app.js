753751c16cf74223f8d14469fb8ba08c135a3bd9
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-04-19 23:18:57 -0400

    added src directory and require condition at top of app file to generate the webpage from a different file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 // const printProfileData = profileDataArr => {
 
@@ -25,30 +27,12 @@ const github = profileDataArgs[1];
 //template literals allow us to do multi-line text easier than regular strings
 // const generatePage = (userName, githubName) => `Name ${userName}, Github: ${githubName}`;
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', generatePage(name, github), err => {
     
-    if (err) throw err;
+    if (err) throw new Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output!')
 });
